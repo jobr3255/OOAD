e8192f9abf4d285b8f711a858d97f555336fbcbb
Josh Brown <jobr3255@colorado.edu>
2020-01-31 11:14:08 -0700

    Updated project.sh options

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # OOAD
+Chloe/Josh Brown
 
 [Project 1](Project1/README.md)
+
+### Compiling and unning projects
+You can easily run each project from the root directory by running the project.sh script and pass in the number of which project you want to run.
+```
+./project.sh PROJECT_NUMBER
+```
+You can also compile and/or run project separately using the -c and -r flags.
+```
+./project.sh PROJECT_NUMBER -c
+./project.sh PROJECT_NUMBER -r
+```
+You can use the -o flag to pipe the output to a specified file.
+```
+./project.sh PROJECT_NUMBER -o FILE_NAME
+```
+You can also use the -h flag for more help.
+```
+./project.sh -h
+```
--- project.sh
+++ project.sh
@@ -6,16 +6,35 @@ END='\e[38;5;231m'
 
 PROJECT_NUM=$1
 PROJECT_DIR=$(pwd)
+FILE_OUTPUT=""
+
+# *************************
+# FUNCTIONS
+# *************************
+
+usage () {
+  echo "usage: ./project.sh [project number] [-c] [-r] [-o] [-h]"
+}
 
 helpMenu () {
-  echo "project.sh - easily compile and run projects"
-  echo " "
-  echo "project.sh [project number] [option]"
-  echo "Defualt options: -c -r"
+  usage
   echo ""
-  echo "[option]:"
-  echo "-c      Compile java project"
-  echo "-r      Run java project"
+  echo "project.sh - easily compile and run projects"
+  echo ""
+  echo "-h"
+  echo "    Display this menu"
+  echo ""
+  echo "[project number]"
+  echo "    Compile and run java project"
+  echo ""
+  echo "[project number] -c"
+  echo "    Compile java project"
+  echo ""
+  echo "[project number] -r"
+  echo "    Run java project"
+  echo ""
+  echo "[project number] -o [file name]"
+  echo "    Compile and run project and output to file. Output file will be located in the project directory"
   echo ""
   exit 0
 }
@@ -27,42 +46,61 @@ compile(){
   if [ $? == 0 ]; then
     echo -e $GREEN"Compile successful"$END
   else
-    echo -e $ERROR" Failed to compile"$END
+    echo -e "$ERROR Failed to compile Project $PROJECT_NUM"
     exit 1
   fi
 }
 
 run(){
-  echo "-------------------------"
+  echo -e $END"-------------------------"
   echo "Running Project $PROJECT_NUM..."
   echo "-------------------------"
-  java -cp $PROJECT_DIR/classes project$PROJECT_NUM.Main
+  if [ ! -z "$FILE_OUTPUT" ]; then
+    echo "Output file: $FILE_OUTPUT"
+    java -cp $PROJECT_DIR/classes project$PROJECT_NUM.Main > $FILE_OUTPUT
+  else
+    java -cp $PROJECT_DIR/classes project$PROJECT_NUM.Main
+  fi
 }
 
+# *************************
+# PRECHECK
+# *************************
 
-# Check options
-if [ $# == 0 ]; then
-  echo -e "$ERROR: No project specified"
-  exit 1
-elif [ $# -gt 2 ]; then
-  echo -e "$ERROR: Too many options"
-  exit 1
-elif [ $1 == "-h" ]; then
+if [[ $* == *-h* ]]; then
   helpMenu
 fi
 
+# Check if project has been spcified
+if [ $# == 0 ]; then
+  echo -e "$ERROR No project specified"
+  exit 1
+fi
+
+# First variable should always be the project number
 PROJECT_DIR=$(pwd)'/Project'$1
 if [ ! -d $PROJECT_DIR ]; then
   echo "Project '$1' does not exists."
   exit 0
 fi
 
+# If only project number is passed in compile and run that project
 if [ $# == 1 ]; then
   compile
   run
 fi
-# If statement to detect flags
-# if [[ $* == *--flag* ]]
+
+# Don't allow compile and run flags to be combined with output flag
+if [[ $* == *-c* || $* == *-r* ]]; then
+  if [[ $* == *-o* ]]; then
+    echo -e "$ERROR '-o' cannot be combined with '-c' or '-r'"
+  fi
+fi
+
+# *************************
+# EXECUTE
+# *************************
+
 while test $# -gt 0; do
   shift
   case "$1" in
@@ -76,10 +114,23 @@ while test $# -gt 0; do
     -r)
       run
       ;;
+    -o)
+      shift
+      if test $# -gt 0; then
+        FILE_OUTPUT="$PROJECT_DIR/$1"
+      else
+        echo -e $ERROR" No output file specified"
+        exit 1
+      fi
+      shift
+      compile
+      run
+      ;;
     *)
       # makes sure the detected variable is not empty
       if [ ! -z "$1" ]; then
         echo "Invalid option: '$1'"
+        usage
         exit 1
       fi
       ;;
